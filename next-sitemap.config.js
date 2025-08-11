6d31d2bba8edf2631e8fb17ef07ddca76b152ea1
Sawasdee7123 <marc@mallolas.com>
2025-08-11 15:03:48 +0800

    Unindex Widget

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,7 @@ module.exports = {
   siteUrl: 'https://mysleepcalculator.net',
   generateRobotsTxt: true, // Generate robots.txt
   sitemapSize: 5000,
+  exclude: [
+    '/widget' // This path will not appear in the sitemap
+  ],
 };
